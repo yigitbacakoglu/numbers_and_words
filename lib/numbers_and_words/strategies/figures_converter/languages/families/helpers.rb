8ccef299b937d1b38ec672ed62babfb7c5a1eaa5
Yigit C. Bacakoglu <yigitcan_bacakoglu@hotmail.com>
2020-03-12 10:03:47 +0300

    Update helpers.rb
    1.9.3

--- lib/numbers_and_words/strategies/figures_converter/languages/families/helpers.rb
+++ lib/numbers_and_words/strategies/figures_converter/languages/families/helpers.rb
@@ -11,7 +11,7 @@ module NumbersAndWords
               translate :zero, options
             end
 
-            %i[ones teens tens tens_with_ones hundreds].each do |method_name|
+            %w[ones teens tens tens_with_ones hundreds].each do |method_name|
               define_method(method_name) do |*args|
                 options = args.first || {}
                 translate method_name, @figures.send(method_name), options
